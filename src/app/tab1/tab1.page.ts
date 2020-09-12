db72dba771ad97f8344f261a5fe0f767f9a10af6
joao junior <juniorgtr123@gmail.com>
2020-09-12 17:56:49 -0300

    aprendizado em Ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
   constructor() {}
-
 }
